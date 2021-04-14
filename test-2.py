5c266554292aa415ca7170575f56c81be2b87e46
mmecoco <jooyoung.kim@epitech.eu>
2021-04-14 21:50:17 +0900

    .

--- test-2.py
+++ test-2.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 class Annimal():
     def __init__(self):
@@ -32,6 +33,8 @@ class Mammalia(Annimal):
     def eat(self,food):
         if(isinstance(food,Pisces)):
             food.die()
+            print("eating pisces")
+
     def reproduce(self,annimal):
         if(isinstance(annimal,Mammalia)):
             new_mammalia = Mammalia()
@@ -41,6 +44,8 @@ class Pisces(Annimal):
     def eat(self,food):
         if(isinstance(food,Birds)):
             food.die()
+            print("eating birds")
+
     def reproduce(self,annimal):
         if(isinstance(annimal,Pisces)):
             new_pisces = Pisces()
@@ -49,6 +54,8 @@ class Birds(Annimal):
     def eat(self,food):
         if(isinstance(food,Reptiles)):
             food.die()
+            print("eating reptiles")
+
     def reproduce(self,annimal):
         if(isinstance(annimal,Birds)):
             new_birds = Birds()
@@ -56,7 +63,9 @@ class Birds(Annimal):
 class Reptiles(Annimal):
     def eat(self,food):
         if(isinstance(food,Mammalia)):
+            print("eating mamalia")
             food.die()
+
     def reproduce(self,annimal):
         if(isinstance(annimal,Reptiles)):
             new_reptiles = Reptiles()
@@ -75,21 +84,23 @@ class Earth():
         
     
     def eat_phase(self):
-        for a in range(int(len(self.annimal)/2)):
-            self.annimal[a].eat(self.annimal[a+int(len(self.annimal)/2)])
+        for a in range(int(len(self.annimal)/2) - 1):
+            self.annimal[a].eat(self.annimal[a+int(len(self.annimal)/2) - 1])
+   
     
     def check_die_annimal(self):
+        self.annimal = [a for a in self.annimal if a != None] # not useful
         updated_annimal = [a for a in self.annimal if a.if_die() == False]
         del self.annimal
         self.annimal = updated_annimal
 
-
-
     def loop(self):
         while(True):
+            random.shuffle(self.annimal)
             self.reproduce_phase()
             self.eat_phase()
             self.check_die_annimal()
+            print(len(self.annimal))
                 
 earth = Earth()
 fish = Pisces()
@@ -97,14 +108,21 @@ human = Mammalia()
 sparrow = Birds()
 lizard = Reptiles()
 earth.add_annimal(fish)
-earth.add_annimal(fish)
-earth.add_annimal(human)
 earth.add_annimal(human)
 earth.add_annimal(sparrow)
-earth.add_annimal(sparrow)
 earth.add_annimal(lizard)
-earth.add_annimal(lizard)
+earth1 = Earth()
+fish1 = Pisces()
+human1 = Mammalia()
+sparrow1 = Birds()
+lizard1 = Reptiles()
+earth.add_annimal(fish1)
+earth.add_annimal(human1)
+earth.add_annimal(sparrow1)
+earth.add_annimal(lizard1)
 earth.loop()
+
+# [p, m, b, r]
 
 
 # 재귀함수
